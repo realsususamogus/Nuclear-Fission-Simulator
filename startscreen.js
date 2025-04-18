54f75d160c3b267c971a286eedbc5104d50be7b8
sususamogus <144018043+realsususamogus@users.noreply.github.com>
2025-04-19 00:08:23 +0800

    Update startscreen.js

--- startscreen.js
+++ startscreen.js
@@ -7,11 +7,6 @@ let neutronspeedslider;
 
 let sizewidthselect; 
 let sizeheightselect;
-let fissilematerialselect;
-
-let fragmentcountslider; 
-
-let fragmentfissionswitch; 
 
 function preload() {
   bgpic = loadImage("explosion.jpg");
@@ -53,27 +48,15 @@ function rerunbutton() {
 }
 
 function customisation() {
-  maxmaterialslider = createSlider(200, 3200, 800)
+  maxmaterialslider = createSlider(200, 1600, 800)
   maxmaterialslider.position(3*width/5 + 20, height/3)
- 
-  neutronspeedslider = createSlider(0.1, 5, 2.0, 0.1)
-  neutronspeedslider.position(3*width/5 + 20, height/3 + 40)
-  
   neutronsreleasedslider = createSlider(1, 5, 1)
-  neutronsreleasedslider.position(3*width/5 + 20, height/3 +80)
-  
-  
-  fragmentcountslider = createSlider(1, 5, fragmentcount)
-  fragmentcountslider.position(3*width/5 + 20, height/3 + 120)
-  
-  fragmentfissionswitch = createCheckbox('Enable fission of fragments (Performance intensive)', false)
-  fragmentfissionswitch.position(3*width/5 + 20, height/3 + 160)
-  
-  
+  neutronsreleasedslider.position(3*width/5 + 20, height/3 +40)
+  neutronspeedslider = createSlider(0.1, 5, 2.0, 0.1)
+  neutronspeedslider.position(3*width/5 + 20, height/3 + 80)
   
   sizewidthselect = createSelect()
   sizewidthselect.position(3*width/5 + 20, height/3 - 50)
-  sizewidthselect.size(100,30)
   sizewidthselect.option('default', 3*width/5)
   sizewidthselect.option('small', width/2)
   sizewidthselect.option('x-small', width/3)
@@ -81,33 +64,16 @@ function customisation() {
   sizewidthselect.selected('default')
   
   sizeheightselect = createSelect()
-  sizeheightselect.size(100,30)
   sizeheightselect.position(3*width/5 + 150, height/3 - 50)
   sizeheightselect.option('default.', height)
   sizeheightselect.option('small.', 2*height/3)
   sizeheightselect.option('x-small.', height/2)
   sizeheightselect.option('xx-small.', height/3)
   sizeheightselect.selected('default.')
-  
-  fissilematerialselect = createSelect()
-  fissilematerialselect.size(120,30)
-  fissilematerialselect.position(3*width/5 + 150, height/3 - 130)
-  fissilematerialselect.option('U-235(default)', 2)
-  fissilematerialselect.option('Pu-239', 3)
-  fissilematerialselect.option('U-233', 2)
-  fissilematerialselect.option('Custom', 1)
-  fissilematerialselect.selected('U-235(default)')
-  
-  
-    
-  
 }
 
-
-
-
 function rerunfunc() {
-  for (let i = fissileMaterial.length; i > -1; i--) {
+  for (let i = fissileMaterial.length; i > 0; i--) {
     fissileMaterial.splice(i, 1)
   }
   for (let i = neutrons.length; i > -1; i--) {
@@ -117,18 +83,12 @@ function rerunfunc() {
   for (let i = fissionFragments.length; i > -1; i--) {
     fissionFragments.splice(i, 1)
   }
-  for (let i = fissionFragments2.length; i > -1; i--) {
-    fissionFragments2.splice(i, 1)
-  }
+  
   for (let i = fissileMaterial.length; i < MAX_MATERIAL; i++) {
     fissileMaterial.push(createVector(random(sizewidthselect.selected()), random(sizeheightselect.selected())));
-    
 }
   neutrontotal = 0
   energy = 0
-  timerstart = false
-  oldtimer = round(timer/30,2)
-  timer = 0
 }
 function changecolor1() {
   //setting the colour change and the shadow of the buttons upon mouseOver
